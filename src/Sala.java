f16b9e64563f955efb35780ebde48fb4329a9c65
Max_ <Max_@10.8.77.2>
2017-08-18 11:14:39 -0300

    Creados los metodos Agregar y Mostrar en la Clase Sala

--- src/Sala.java
+++ src/Sala.java
@@ -1,3 +1,5 @@
+
+import java.io.*;
 import java.util.*;
 
 public class Sala {
@@ -13,6 +15,10 @@ public class Sala {
 		
 		this.id = id;
 		this.asientos = asientos;
+	}
+
+	public Sala() {
+		// TODO Auto-generated constructor stub
 	}
 
 	//Métodos
@@ -32,11 +38,45 @@ public class Sala {
 	public void setAsientos(ArrayList<Asiento> asientos) {
 		this.asientos = asientos;
 	}
+	ArrayList<Sala> salas = new ArrayList<Sala>();
+	Sala objSala = new Sala();
+	public void agregarSala(String id) throws IOException{
+		
+		int i=1 ;
+		String sala ; 
+		BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+		do{
+			
+			System.out.println(" Desea agregar sala ");
+			sala = lector.readLine();
+			objSala.setId(sala);
+			salas.add(objSala);
+			// Agregar llamada a metodo de agregar asientos 
+			
+			do{
+			System.out.println("¿Desea Agregar otra Sala?, si ingrese 1 sino ingrese 0");
+			i= Integer.parseInt(lector.readLine());
+			}while(i>-1 && i<2);
+		
+		}while(i==1);
+	}
+
+
+	public void mostrarTodasSalas(){
+		System.out.println("Las Salas existentes Son:");
+		for(int i=0;i<salas.size();i++)
+		{
+			if(salas.isEmpty())
+			{
+				System.out.println("no existen Salas creadas");
+			}
+			else
+			{
+				System.out.println(salas.get(i));
+			}
 	
 	
 	
-	
-	
-	
-	
+		}
+	}
 }
